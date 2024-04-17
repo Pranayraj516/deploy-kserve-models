a9b2538631ddfdec960975f4afaf027d9e80d7be
Pranayraj516 <pranayraj.mangala@oneconvergence.com>
2024-04-17 20:44:21 +0530

    changes in kserve client file

--- kserve_client.py
+++ kserve_client.py
@@ -1,44 +1,30 @@
 # Usage: python kserve_client.py <profile> <name> <input json file name>
 # Ex: python kserve_client.py default sklearnv1 input.json
 
-
-import configparser
 import requests
 import sys
 import urllib3
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-config = configparser.ConfigParser()
-
-
 # Check if a parameter is provided
 if len(sys.argv) > 3:
-    param1 = sys.argv[1]
-    param2 = sys.argv[2]
-    param3 = sys.argv[3]
+    param1 = sys.argv[1] 
 else:
     print("Please provide name.")
     exit(1)
 
-# get http url & token
-ini_file = "/userdata/.d3x.ini"
-config.read(ini_file)
-url = config.get(param1,"url")
-token = config.get(param1,"auth-token")
-
-
-# get deployment details
-headers = {'Authorization': token}
-r = requests.get(f"{url}/llm/api/deployments/{param2}", headers=headers, verify=False)
-deployment = r.json()['deployment']
-
 # get serving details
-SERVING_TOKEN = deployment['serving_token']
-SERVING_ENDPOINT = f"{url}{deployment['endpoint']}"
+SERVING_TOKEN = sys.argv[2]
+SERVING_ENDPOINT = sys.argv[3]
+print(SERVING_TOKEN)
+print("\n")
+print(SERVING_ENDPOINT)
+print("\n")
 
 # serving request
 headers={'Authorization': SERVING_TOKEN, 'Content-Type': 'application/json'}
-resp = requests.post(SERVING_ENDPOINT, data=open(param3, 'rb'), headers=headers, verify=False)
+print(headers)
+print("\n")
+resp = requests.post(SERVING_ENDPOINT, data=open(param1, 'rb'), headers=headers, verify=False)
 print (resp.text)
-
