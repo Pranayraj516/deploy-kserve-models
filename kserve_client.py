b58d64f262ba7d20a1c44b87006de5c1e0f44aea
Pranayraj516 <pranayraj.mangala@oneconvergence.com>
2024-04-18 12:55:34 +0530

    changes in kserve client file

--- kserve_client.py
+++ kserve_client.py
@@ -9,16 +9,14 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 # Check if a parameter is provided
 if len(sys.argv) > 3:
-    param1 = sys.argv[1] 
+    INPUT_JSON = sys.argv[1]
+    SERVING_TOKEN = sys.argv[2]
+    SERVING_ENDPOINT = sys.argv[3]
 else:
     print("Please pass valid arguments.")
     exit(1)
 
-# get serving details
-SERVING_TOKEN = sys.argv[2]
-SERVING_ENDPOINT = sys.argv[3]
-
 # serving request
 headers={'Authorization': SERVING_TOKEN, 'Content-Type': 'application/json'}
-resp = requests.post(SERVING_ENDPOINT, data=open(param1, 'rb'), headers=headers, verify=False)
+resp = requests.post(SERVING_ENDPOINT, data=open(INPUT_JSON, 'rb'), headers=headers, verify=False)
 print (resp.text)
